3b2181db6a98173b59122a7b7eb1b6bddf7b4175
Christoph Hoffmann <Christoph.Hoffmann@oeaw.ac.at>
2017-11-08 16:15:20 +0100

    projects updated

--- src/components/projects.vue
+++ src/components/projects.vue
@@ -1,10 +1,10 @@
 <template>
   <v-content v-if="!loading">
     <section>
-      <v-parallax src="static/savoyen2.jpg" height="380">
+      <v-parallax  v-bind:src="splash.banner.url" height="380">
         <v-layout column align-center justify-center>
           <div class="layer"></div>
-          <h4 class="white--text pagecaption">Projects</h4>
+          <h4 class="white--text pagecaption">{{ splash.headline }}</h4>
         </v-layout>
       </v-parallax>
       <v-layout
@@ -15,11 +15,28 @@
       >
         <v-flex xs12 sm4 class="my-3">
           <div class="text-xs-center">
-            <span class="subheading" v-html=""></span>
+            <span class="subheading" v-html="splash.mainContentOfPage"></span>
           </div>
         </v-flex>
       </v-layout>
     </section>
+    <v-flex xs12>
+      <v-container grid-list-xl>
+        <v-layout row wrap align-center>
+          <v-flex xs12 md4 v-for="project in projects" :key="project.name">
+            <v-card class="elevation-0 transparent">
+              <v-card-media v-bind:src="project.thumbnailUrl.url" height="200px">
+              </v-card-media>
+              <v-card-title primary-title class="layout justify-center">
+                <div class="headline text-xs-center" height="200px!important">{{ project.headline }}</div>
+              </v-card-title>
+              <v-card-text class="text-xs-center" v-html="project.description">
+              </v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-flex>
   </v-content>
 </template>
 
@@ -30,8 +47,8 @@ export default {
   mixins: [DRUPAL],
   data: () => ({
     toFetch: {
-      menu: 'menu',
-      mission: 'full\\9',
+      splash: 'full\\22',
+      projects: 'teaser\\?type=project',
     },
     windowSize: {
       x: window.innerWidth,
@@ -43,7 +60,7 @@ export default {
   created() {
     /* eslint no-console: ["error", { allow: ["log"] }] */
     this.batchget(this.toFetch).then((res) => {
-      console.log(this.menu, res);
+      console.log(this.projects, res);
     });
   },
   mounted() {
