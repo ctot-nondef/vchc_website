3e943eaf9adec1e530a0480091ea80f71d232291
Christoph <christoph.hoffmann@oeaw.ac.at>
2018-01-31 15:27:40 +0100

    layout fixes

--- src/components/app.vue
+++ src/components/app.vue
@@ -49,16 +49,16 @@
       </main>
       <v-footer color="primary"  app v-if="!loading">
         <v-layout column>
-            <v-flex xs12>
+            <v-flex xs12 class="mt-5 mb-5">
               <v-layout row wrap>
-                <v-flex v-for="logo in footer.imagefull" xs12 sm6 md3 class="text-xs-center" >
+                <v-flex v-for="logo in footer.imagefull" xs12 sm6 md3 class="text-xs-center mt-5 mb-5" >
                   <img align-center :src="logo.url" style="max-height:150px;max-width:150px;" >
                 </v-flex>
               </v-layout>
             </v-flex>
-            <v-flex xs12>
+            <v-flex xs12 class="mt-5 mb-5">
               <v-layout row wrap  >
-                <v-flex v-for="item in footermenu" xs12 sm6  class="text-xs-center">
+                <v-flex v-for="item in footermenu" xs12 sm6  class="text-xs-center" >
                   <v-btn color="white" flat  :key="item.tid[0].value" :to="{name: item.field_path[0].value}">{{ item.name[0].value }}</v-btn>
                 </v-flex>
               </v-layout>
