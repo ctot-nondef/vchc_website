75264a63aa116113d065abba8b485940f15c0277
Christoph Hoffmann <Christoph.Hoffmann@oeaw.ac.at>
2018-08-20 15:46:52 +0200

    backend moved to apollo

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const HTTP = axios;
 
 const DRUPAL = axios.create({
-  baseURL: 'https://wksd8dev.eos.arz.oeaw.ac.at/',
+  baseURL: 'https://wks-backend.apollo.arz.oeaw.ac.at/',
   params: {
     _format: 'json',
   },
